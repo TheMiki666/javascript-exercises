1d3f846ef7e5cac1c7d09c50787f13d9c0f7c35e
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-09-21 19:37:19 +0200

    EXERCISE 08 DONE

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x+y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x-y;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((x,y)=>x+y, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce((x,y)=>x*y, 1);
 };
 
-const power = function() {
-	
+const power = function(x,y) {
+	return x**y;
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if (x<=1){
+    return 1;
+  }else{
+    return x*factorial (x-1);
+  }
 };
 
 // Do not edit below this line
