95c2847660f62b89288bc6d6cfc39979d2914eed
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-09-23 09:49:50 +0200

    PALINDROMES DONE

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    //Phase 1: filter
+    let wordFilter="";
+    for (let i=0;i<word.length;i++){
+        let char=word.charAt(i);
+        if ((char>="a" && char<="z")||  (char>="A" && char<="Z")|| (char>="0" && char<="9")){
+            char=char.toLowerCase();
+            wordFilter=wordFilter.concat(char);
+        }
+    }
+    
+    //Phase 2 check if it is palindrome
 
+    let reflect="";
+    for (let i=wordFilter.length-1;i>=0;i--){
+        reflect=reflect.concat(wordFilter.charAt(i));      
+    }
+    return reflect==wordFilter;
 };
+
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
