eddef8ae04cd029d5572f982b801c1486a6bab6d
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-09-23 10:34:09 +0200

    fibonacci DONE

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,22 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num=parseInt(num);
+    if (isNaN(num) || num <0){
+        return "OOPS";
+    }else if (num==0){
+        return 0;
+    }else if (num==1 || num==2){
+        return 1;
+    }else{
+        let sum1=1;
+        let sum2=1;
+        let aux;
+        for (let i=3; i<=num; i++){
+            aux=sum1;
+            sum1=sum2;
+            sum2=sum2+aux;
+        }
+        return sum2;
+    }
 };
 
 // Do not edit below this line
