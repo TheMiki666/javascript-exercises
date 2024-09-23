7ecd049909f88b780b45dccf50146ad1a0f036f3
TheMiki666 <178334893+TheMiki666@users.noreply.github.com>
2024-09-23 22:52:56 +0200

    EXERCISE 12 DONE
    All the exercises finished

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,48 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    let response={};
+    let oldestAge=0;
+    let today= new Date ().getFullYear(); 
+    for (let person of people){
+        let age;
+        if (typeof person.yearOfDeath==="undefined"){
+            //Living person
+            age=today-person.yearOfBirth;
+            
+        }else{
+            //Dead person
+            
+            age=person.yearOfDeath-person.yearOfBirth;
+            
+        }
+        if (age>oldestAge){
+            oldestAge=age;
+            response=person;
+        }
+    }
+    return response;
 };
+/*const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ];
+
+console.log(findTheOldest (people));*/
+
+
+
 
 // Do not edit below this line
 module.exports = findTheOldest;
